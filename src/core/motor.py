8f4831cc9118cc50b377ccc593fa0f0eec10e179
jouramie <jouramie@hotmail.com>
2022-11-28 21:14:57 -0500

    pyautogui bad

--- src/core/motor.py
+++ src/core/motor.py
@@ -1,8 +1,7 @@
-import pyautogui
+import keyboard
 import win32com.client as comclt
 
 import properties
-from util.profiling import timeit
 
 _last_direction: None | str = None
 
@@ -27,19 +26,9 @@ def turn(rotation: float):
 
     if _last_direction != new_direction:
         if _last_direction is not None:
-            pyautogui.keyUp(_last_direction)
+            keyboard.release(_last_direction)
 
         if new_direction is not None:
-            pyautogui.keyDown(new_direction)
+            keyboard.press(new_direction)
 
     _last_direction = new_direction
-
-
-@timeit(name="press", print_each_call=True)
-def press():
-
-    wsh.AppActivate(properties.GAME_WINDOW_TITLE)  # select another application
-    wsh.SendKeys("{LEFT}")  # send the keys you want
-
-
-# press()
