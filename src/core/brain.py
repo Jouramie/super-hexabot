ea3b13a96655b3668f57d07fcb9f8c45bae40e39
Jouramie <jouramie@hotmail.com>
2023-08-17 21:37:19 -0400

    eliminate unreachable directions

--- src/core/brain.py
+++ src/core/brain.py
@@ -13,6 +13,16 @@ def choose_direction(position: float, available_distances: list[int]) -> float:
     :return: the direction where to turn to get to the ideal position to avoid the obstacles, from -1 to 1 where 1 would be a 180 degrees turn clockwise
     """
     approximated_index = (position + 1) * len(available_distances) / 2
+
+    reachable = calculate_reachable(round(approximated_index), available_distances)
+    reachable_distances = {i: available_distances[i] for i in reachable}
+    logger.info(f"Reachable directions are {reachable_distances} from {approximated_index}")
+    if len(reachable) > 1:
+        reachable_available_distances = [j if i in reachable else 0 for i, j in enumerate(available_distances)]
+    else:
+        reachable_available_distances = available_distances
+        logger.warning(f"Ohno! Found no reachable directions from {approximated_index}...")
+
     max_available_distance = max(available_distances)
     obstacle_count = len(available_distances)
 
@@ -20,7 +30,7 @@ def choose_direction(position: float, available_distances: list[int]) -> float:
     #    obst * (1 - abs(calculate_turn(position, to_circle_percent(index, obstacle_count)))) for index, obst in enumerate(available_distances)
     # ]
 
-    normalized_obstacles = [round(d * properties.BRAIN_CATEGORIZING / max_available_distance) for d in available_distances]
+    normalized_obstacles = [round(d * properties.BRAIN_CATEGORIZING / max_available_distance) for d in reachable_available_distances]
 
     max_value = max(normalized_obstacles)
     index_reaching_max = [i for i, j in enumerate(normalized_obstacles) if j == max_value]
@@ -43,6 +53,27 @@ def calculate_turn(position: float, target: float) -> float:
         direction += -2
 
     return direction
+
+
+def calculate_reachable(approximated_index: int, normalized_obstacles: list[int]) -> set[int]:
+    reachable = {approximated_index % len(normalized_obstacles)}
+    for obstacle_index in range(approximated_index + 1, len(normalized_obstacles) + approximated_index):
+        i = obstacle_index if obstacle_index < len(normalized_obstacles) else obstacle_index - len(normalized_obstacles)
+        dist = normalized_obstacles[i]
+        if dist <= properties.BRAIN_MINIMAL_SPACE:
+            break
+
+        reachable.add(i)
+
+    for obstacle_index in range(len(normalized_obstacles) + approximated_index - 1, approximated_index, -1):
+        i = obstacle_index if obstacle_index < len(normalized_obstacles) else obstacle_index - len(normalized_obstacles)
+        dist = normalized_obstacles[i]
+        if dist <= properties.BRAIN_MINIMAL_SPACE:
+            break
+
+        reachable.add(i)
+
+    return reachable
 
 
 def to_circle_percent(index: int, obstacle_count: int) -> float:
--- test/core/brain_test.py
+++ test/core/brain_test.py
@@ -22,7 +22,7 @@ class TestBrain(TestCase):
 
     def test_given_best_spot_is_far_left_when_choose_direction_then_turn_left(self):
         position = 0
-        distances = [0, 100, 0, 0, 0, 0, 0, 0]
+        distances = [0, 100, 50, 50, 0, 0, 0, 0]
 
         chosen_direction = brain.choose_direction(position, distances)
 
@@ -30,7 +30,7 @@ class TestBrain(TestCase):
 
     def test_given_best_spot_is_opposite_when_choose_direction_then_turn_left(self):
         position = 0
-        distances = [100, 0, 0, 0, 0, 0, 0, 0]
+        distances = [100, 50, 50, 50, 50, 0, 0, 0]
 
         chosen_direction = brain.choose_direction(position, distances)
 
@@ -38,7 +38,7 @@ class TestBrain(TestCase):
 
     def test_given_player_is_right_and_best_spot_is_up_when_choose_direction_then_turn_left(self):
         position = 0.5
-        distances = [0, 0, 0, 0, 100, 0, 0, 0]
+        distances = [0, 0, 50, 50, 100, 50, 50, 0]
 
         chosen_direction = brain.choose_direction(position, distances)
 
@@ -46,7 +46,7 @@ class TestBrain(TestCase):
 
     def test_given_player_is_far_left_and_best_spot_is_far_right_when_choose_direction_then_turn_left(self):
         position = -0.75
-        distances = [0, 0, 0, 0, 0, 0, 0, 100]
+        distances = [50, 50, 50, 0, 0, 0, 0, 100]
 
         chosen_direction = brain.choose_direction(position, distances)
 
@@ -67,3 +67,19 @@ class TestBrain(TestCase):
         chosen_direction = brain.choose_direction(position, distances)
 
         self.assertAlmostEqual(chosen_direction, -0.25, delta=0.05)
+
+    def test_given_best_spot_right_but_right_is_blocked__when_choose_direction_then_turn_left(self):
+        position = 0
+        distances = [0, 50, 20, 20, 0, 0, 100, 0]
+
+        chosen_direction = brain.choose_direction(position, distances)
+
+        self.assertAlmostEqual(chosen_direction, -0.75, delta=0.05)
+
+    def test_given_best_spot_left_but_left_is_blocked__when_choose_direction_then_turn_right(self):
+        position = 0.75
+        distances = [50, 100, 50, 50, 100, 0, 0, 0]
+
+        chosen_direction = brain.choose_direction(position, distances)
+
+        self.assertAlmostEqual(chosen_direction, 0.50, delta=0.05)
